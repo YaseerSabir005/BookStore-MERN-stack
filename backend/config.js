9ee437331cc4b0457ab7619ff66dbf2efa490fb5
Yaseer Sabir <yaseersabir005@gmail.com>
2024-03-16 14:42:18 -0600

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+    'mongodb+srv://root:toor@books-store-mern.2iuitjb.mongodb.net/books-collection?retryWrites=true&w=majority'
+
+    // Please create a free database for yourself
+    // This database will be deleted after tutorial
